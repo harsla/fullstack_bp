495ad88ac5064197d46be021849db59297133bf7
Steve Truesdell <steven.truesdell@gmail.com>
2014-12-19 11:35:34 -0700

    making structural changes and adding sessions w/ redis

--- models/user.js
+++ models/user.js
@@ -1,19 +1,39 @@
-var mongoose = require('mongoose'),
-    bcrypt = require('bcrypt');
+'use strict';
 
-var userSchema = new mongoose.Schema({
-  name: { type: String, trim: true, required: true },
-  email: { type: String, unique: true, lowercase: true, trim: true },
+var mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
   password: String,
   resetPasswordToken: String,
   resetPasswordExpires: Date,
-  locked: { type: Boolean, default: false}
+  locked: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 userSchema.pre('save', function(next) {
-  'use strict';
   var user = this;
-  if (!user.isModified('password')) return next();
+  if (!user.isModified('password')) {
+    return next();
+  }
   bcrypt.genSalt(12, function(err, salt) {
     if (err) {
       return next(err);
@@ -29,7 +49,6 @@ userSchema.pre('save', function(next) {
 });
 
 userSchema.methods.comparePassword = function(candidatePassword, cb) {
-  'use strict';
   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
     if (err) {
       return cb(err);
